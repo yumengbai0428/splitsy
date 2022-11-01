d598ea93af9c857ec70db2af59d1f612beb8ee74
Your Name <you@example.com>
2022-11-01 10:03:24 -0400

    minor change in user controller, finished rspec test for user_controller_validate

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,7 +2,36 @@ require 'rails_helper'
 require 'support/action_controller_workaround'
 
 describe UsersController, :type => :controller do
-
+    describe "UserController" do
+        render_views
+       
+        context "validate a user" do
+          before :each do
+            user1 = User.create(id: 1, name: 'a', email: 'a@g', password: 'p1', default_currency: '$')
+            user2 = User.create(id: 2,name: 'b', email: 'b@g', password: 'p2', default_currency: 'Yen')
+          end
     
+          it "user not exist" do
+            get :validate, user: {"email": "c@g", "password": "p1"}
+            expect(flash[:notice]).to eq("User login was invalid.")
+            expect(response).to redirect_to('/welcome')
+          end
+
+          it "user exists and correct password" do
+            get :validate, user: {"email": "a@g", "password": "p1"}
+          
+            expect(session[:user_email]).to eq("a@g")
+            expect(response).to redirect_to(transactions_path)
+          end
+
+          it "user exists but incorrect password" do
+            get :validate, user: {"email": "a@g", "password": "p2"}
+          
+            expect(flash[:notice]).to eq("User login was invalid.")
+            expect(response).to redirect_to('/welcome')
+          end
+
+        end
+    end
 end
 
