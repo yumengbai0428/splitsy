4143955b5cd201e9f7936cbeb0ed97b398f6436c
Your Name <you@example.com>
2022-11-14 17:25:34 -0500

    added transaction model tests

--- db/migrate/20221111224403_add_tag_to_transactions.rb
+++ db/migrate/20221111224403_add_tag_to_transactions.rb
@@ -1,5 +1,5 @@
 class AddTagToTransactions < ActiveRecord::Migration
   def change
-    add_column :transactions, :tag, :string :default => "expenditure"
+    add_column :transactions, :tag, :string, :default => "expenditure"
   end
 end
