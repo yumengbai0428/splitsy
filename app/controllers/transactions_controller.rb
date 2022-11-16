ad06b29917ac11784d1d150e9ce987c5108112cd
Harshini <52701436+harshiniwho@users.noreply.github.com>
2022-11-16 16:08:56 -0500

    bug fix on filter (#43)

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -56,10 +56,18 @@ class TransactionsController < ApplicationController
 
     def list
         @user_email = session[:user_email]
-        if params["filter_form"].nil? || (params["filter_form"]["tag"].nil? || params["filter_form"]["tag"] == "") &&  (params["filter_form"]["start_date"].nil? && params["filter_form"]["start_date"] != "") && (params["filter_form"]["end_date"].nil? && params["filter_form"]["end_date"] != "")
+        filter_form = params["filter_form"]
+        if filter_form.nil?
             @transactions = Transaction.all_transactions_for_user(session[:user_email])
-        else
-            @transactions = Transaction.find_tansactions_tag_time(session[:user_email],  params["filter_form"]["tag"], params["filter_form"]["start_date"], params["filter_form"]["end_date"])
+        else 
+            tag = params["filter_form"]["tag"]
+            start_date = params["filter_form"]["start_date"]
+            end_date = params["filter_form"]["end_date"]
+            if (tag.nil? or tag.empty?) and (start_date.nil? or start_date.empty?) and (end_date.nil? or end_date.empty?)
+                @transactions = Transaction.all_transactions_for_user(session[:user_email])
+            else
+                @transactions = Transaction.find_tansactions_tag_time(session[:user_email],  params["filter_form"]["tag"], params["filter_form"]["start_date"], params["filter_form"]["end_date"])
+            end
         end
     end
 
